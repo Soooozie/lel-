8efb6808a697966f81e648f20d058089be687417
root <root@localhost.localdomain>
2016-02-07 17:41:27 -0500

    Adding the Comic template file

---
+++ comic.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * The template for displaying comics.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package LeL
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<div id="content" class="site-content" role="main">
+
+			<?php
+			if ( have_posts() ):
+				while (have_posts() ): the_post();
+
+				get_template_part( 'template-parts/content', 'template-parts/comic' );
+
+			endwhile;
+
+		else:
+
+			get_template_part('no-results', 'index');
+		endif;
+			?>
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();?>
